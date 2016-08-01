99b36763e9a956363caab231cf7f8bfc8fa326ff
qwe qwe <efor@ngs.ru>
2016-08-01 08:31:04 +0000

    решен транспорт деплоя + внесены правки в мелкие файлы + добавлена и расширена  инструкция по эксплуатации

--- shipitfile.js
+++ shipitfile.js
@@ -1,23 +1,59 @@
-// подключение ключа деплоя
-module.exports = function (shipit) {
-  require('shipit-deploy')(shipit);
 
-  shipit.initConfig({
-    default: {
-      workspace: '/tmp/github-monitor',
-      deployTo: '/tmp/deploy_to',
-      repositoryUrl: 'https://github.com/user/repo.git',
-      ignores: ['.git', 'node_modules'],
-      rsync: ['--del'],
-      keepReleases: 2,
-      key: '/id_rsa_deploy.pub',
-      shallowClone: true
-    },
-    staging: {
-      servers: 'user@myserver.com'
-    }
-  });
-  
+module.exports = function (shipit) {
+	
+	require('shipit-deploy')(shipit);
+
+	shipit.initConfig({
+		default: {
+			workspace: '/tmp/gdetus2/github-monitor',
+			deployTo: '/gdetus2',
+			repositoryUrl: 'https://github.com/eventengine/777777',
+			ignores: ['.git'],
+			keepReleases: 3,
+			key: '/home/ubuntu/.ssh/id_rsa',
+			shallowClone: true
+		},
+		staging: {
+			servers: 'insomakarma@141.8.194.121'
+		}
+	});
+	
+	/**
+	 * Задачи перед началом развертывания приложения.
+	 */
+	shipit.on('deploy', function () {
+		shipit.start('gdetus-stop');
+	});
+	
+	/**
+	 * Задачи после развертывания приложения.
+	 */
+	shipit.on('deployed', function () {
+		shipit.start(['gdetus-prepare', 'gdetus-start']);
+	});
+	
+	/**
+	 * Задача: Останов приложения при помощи PM2.
+	 */
+	shipit.task("gdetus-stop", function() {
+		
+	});
+	
+	/**
+	 * Задача: Подготовка приложения перед запуском.
+	 * Восстановление папки node_modules командой npm install.
+	 */
+	shipit.task("gdetus-prepare", function() {
+		
+	});
+	
+	/**
+	 * Задача: Запуск приложения при помощи PM2.
+	 */
+	shipit.task("gdetus-start", function() {
+		
+	});
+	
 };
 
 
